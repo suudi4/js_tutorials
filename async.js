69573d48aaec9e72990b8f4db8403b1d1f207718
suudi4 <suudi08@gmail.com>
2024-01-13 09:19:21 +0300

    second commit

--- async.js
+++ async.js
@@ -1,7 +1,16 @@
 // Promis ama async/await
 
 function orderCoffee(order) {
+  // code here
   return order;
 }
 
 console.log(orderCoffee("capuchino"));
+
+function orderjoke() {
+  let respone = fetch("https://api.api-ninjas.com/v1/jokes");
+
+  console.log(respone);
+}
+
+orderjoke();
